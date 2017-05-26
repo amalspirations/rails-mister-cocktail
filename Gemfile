51d0d935ee5ee849066bed2004f4f9d61acf574c
Amal Ketata <amal.ketata@edu.escpeurope.eu>
2017-05-26 14:34:42 +0100

    Added home page

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 
 gem 'figaro'
+gem 'cloudinary'
 
 # Gemfile Frontend
 gem 'bootstrap-sass'
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
-  root to: 'cocktails#index'
+  root to: 'pages#home'
 
 end
